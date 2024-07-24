8ef298c63e5efd2c9c2f024eef8ac9361e8b0896
personal <suhaillahmadd0@gmail.com>
2024-07-25 04:18:28 +0530

    - added print statement to debug

--- Sources/cryptotokens-swift/SVGManager.swift
+++ Sources/cryptotokens-swift/SVGManager.swift
@@ -1,11 +1,12 @@
 import Foundation
 
-public struct SVGManager {
-    public static func getSVG(from folder: String, named name: String) -> Data? {
-        let bundle = Bundle.module
-        guard let url = bundle.url(forResource: name, withExtension: "svg", subdirectory: "Resources/\(folder)") else {
-            return nil
-        }
-        return try? Data(contentsOf: url)
+func getSVG(from folder: String, named name: String) -> Data? {
+    let bundle = Bundle.module
+    let resourcePath = "Resources/\(folder)/\(name).svg"
+    guard let url = bundle.url(forResource: name, withExtension: "svg", subdirectory: "Resources/\(folder)") else {
+        print("Resource URL not found at path: \(resourcePath)")
+        return nil
     }
+    print("Loading SVG from URL: \(url)")
+    return try? Data(contentsOf: url)
 }
