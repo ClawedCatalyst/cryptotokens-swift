099622a2a5668a4e091bfdca8f17fea6ce2bdd9d
personal <suhaillahmadd0@gmail.com>
2024-07-25 04:21:09 +0530

    - fix SVGManager

--- Sources/cryptotokens-swift/SVGManager.swift
+++ Sources/cryptotokens-swift/SVGManager.swift
@@ -1,12 +1,14 @@
 import Foundation
 
-func getSVG(from folder: String, named name: String) -> Data? {
-    let bundle = Bundle.module
-    let resourcePath = "Resources/\(folder)/\(name).svg"
-    guard let url = bundle.url(forResource: name, withExtension: "svg", subdirectory: "Resources/\(folder)") else {
-        print("Resource URL not found at path: \(resourcePath)")
-        return nil
+public struct SVGManager {
+    public static func getSVG(from folder: String, named name: String) -> Data? {
+        let bundle = Bundle.module
+        let resourcePath = "Resources/\(folder)/\(name).svg"
+        guard let url = bundle.url(forResource: name, withExtension: "svg", subdirectory: "Resources/\(folder)") else {
+            print("Resource URL not found at path: \(resourcePath)")
+            return nil
+        }
+        print("Loading SVG from URL: \(url)")
+        return try? Data(contentsOf: url)
     }
-    print("Loading SVG from URL: \(url)")
-    return try? Data(contentsOf: url)
 }
