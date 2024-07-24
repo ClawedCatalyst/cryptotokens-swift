253b772a241d4d22c0caaf1a70ee574d09a5f08d
personal <suhaillahmadd0@gmail.com>
2024-07-25 05:09:09 +0530

    - updated package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,18 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "cryptotokens-swift",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "cryptotokens-swift",
             targets: ["cryptotokens-swift"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "cryptotokens-swift",
             resources: [
-                .process("Resources/branded"),
-                .process("Resources/mono")
+                .process("Resources")
             ]),
         .testTarget(
             name: "cryptotokens-swiftTests",
